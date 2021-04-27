bafd5dda551999b4c3bb94bfc6de184d6555f612
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-27 15:24:47 -0400

    creates console log that gives user attraction name, attraction description, and a link to the attractions wikipedia

--- main.js
+++ main.js
@@ -6,14 +6,11 @@ enterAPI.click(function() {
     const apiKey = $(".apikey-input").val();
     //test what details come with the data
     const cityInput = encodeURIComponent($('.city-input').val().toLowerCase());
-    //using USA as example, fetch the url
+    //uses inputs to finish fetch opentripmap url
     fetch(`https://api.opentripmap.com/0.1/en/places/geoname?name=${cityInput}&apikey=${apiKey}`)
         .then((res) => res.json())
         .then((data) => {
-        console.log(data)
-        // console.log("This is the latitude: " + data.lat);
-        // console.log("This is the latitude: " + data.lon);
-
+        //uses data from open trip map to gather coordinates
         const latitude = data.lat;
         const longitude = data.lon;
         const coordinates = [longitude, latitude];
@@ -22,29 +19,33 @@ enterAPI.click(function() {
         .then((coordinates) => {
             const radiusInput = $('.radius-input').val();
             const attractionInput = $('.attraction-input').val().toLowerCase();
-
+            //uses data from open trip map to gather get things within city centers radius
             fetch(`https://api.opentripmap.com/0.1/en/places/radius?radius=${radiusInput}&lon=${coordinates[0]}&lat=${coordinates[1]}&kinds=${attractionInput}&rate=3&limit=5&apikey=${apiKey}`)
                 .then((res) => res.json())
                 .then((data) => {
-                console.log(data.features.length);
-                console.log(data.features[0])
-                // this will target the name of the museums
-                // console.log(data.features[0].properties.name)
-
-                for (let i = 0; i < 1; i++) {
-                    console.log(data.features[i].properties.name)
-                    console.log(data.features[i].properties.kinds)
-                    //below will give you the wikidata id needed for another fetch that will get you the descriptions of the place
-                    // console.log(data.features[i].properties.wikidata)
+                // loops through the data to get wikidata id needed to get more information
+                for (let i = 0; i < 5; i++) {
                     const wikiID = data.features[i].properties.wikidata;
-                    //below will get the wikidata api stuff
+                    //below will get the wikidata api data
                     fetch(`https://www.wikidata.org/wiki/Special:EntityData/${wikiID}.json`)
                         .then((res) => res.json())
                         .then((data) => {
-                            // uses fetch to get the zoos description from wikidata!
+                            // uses data from wikidata api to console log necessary information
+
+                            //gets the name of the place
+                            console.log("Attractions name: ")
+                            console.log(data.entities[Object.keys(data.entities)[0]].labels.en.value)
+
+                            // gets the wikidata description of the place
+                            console.log("description: ")
                             console.log(data.entities[Object.keys(data.entities)[0]].descriptions.en.value)
-                            //trying to get the wikipedia page
+                    
+                            //gets the wikipedia url
+                            console.log("Wikipedia Page: ")
                             console.log(data.entities[Object.keys(data.entities)[0]].sitelinks.enwiki.url)
+                                
+                            
+                            
                         })
                 }
 
